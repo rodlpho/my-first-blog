908587edbf154795554ef6d4a2c47553c9af3c23
rodlpho <rodolphodeales@gmail.com>
2019-03-29 14:09:17 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+print('Hello, Django girls!')
+
+
+def printing(string):
+    if len(string) > 0:
+        print (string)
+    else:
+        print ("nao tenho caractere na funcao")
+
+
+printing('Rodolpho')
+
+printing('')
